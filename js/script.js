a0d9e1741243a345cac3ecf1ae5bebf916ffcf66
brodiewebdt <81633541+brodiewebdt@users.noreply.github.com>
2024-06-14 10:27:56 -0400

    Added reset styles to styles.css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,7 @@
+const shareBar = document.querySelector('#share-bar');
+const shareBtn = document.querySelector('#share-btn');
 
+shareBtn.addEventListener('click', () => {
+  shareBar.classList.toggle('active');
+  shareBtn.classList.toggle('active');
+});
